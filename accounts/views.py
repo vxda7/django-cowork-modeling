3574e6a778393e1c4f0c3a5b6a409ba55fa3bef6
mandaringit <compurain@gmail.com>
2019-11-20 13:55:06 +0900

    유저 인덱스 페이지 + 디테일페이지

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.contrib.auth import get_user_model
 
 # Create your views here.
+def index(request):
+    users = get_user_model().objects.all()
+    context = {
+        'users':users
+    }
+    return render(request,'accounts/index.html',context)
+
+def detail(request,user_pk):
+    user = get_object_or_404(get_user_model(),id=user_pk)
+
+    return  render(request,'accounts/detail.html',{'user':user})
