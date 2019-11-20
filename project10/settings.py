11ca758997337d1ca2144161be313125eac3a1c6
mandaringit <compurain@gmail.com>
2019-11-20 15:04:39 +0900

    Nav 추가

--- project10/settings.py
+++ project10/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'movies',
     'accounts',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
