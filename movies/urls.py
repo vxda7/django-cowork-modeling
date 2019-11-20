bdb7190bf72bc65824ec89a1be49992abd4f5fd5
vxda77 <vxda77@gmail.com>
2019-11-20 13:29:48 +0900

    desktop

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
+
+app_name = "movies"
 
 urlpatterns = [
-
+    path('', views.index, name="index"),
+    path('<int:id>/', views.detail, name="detail"),
+    
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Movie, Genre
+from django.contrib.auth import get_user_model
 # Create your views here.
+
+def index(request):
+    
+    return render(request, 'movies/index.html')
+
+
+def detail(request, id):
+    movie = get_object_or_404(Movie, id=id)
+    context = {
+        'movie': movie,
+    }
+    return render(request, 'movies/detail.html', context)
